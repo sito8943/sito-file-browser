feab5d3ead9cd0010c91b880d830544f14eff747
Sito8943 <sito8943@gmail.com>
2026-09-20 12:01:35 +0200

    remove '/' from volumes

--- src/features/sidebar/components/VolumeItem/VolumeItem.tsx
+++ src/features/sidebar/components/VolumeItem/VolumeItem.tsx
@@ -35,7 +35,7 @@ const VolumeItem = ({
       tabIndex={0}
       data-path={volume.mountPoint}
       aria-current={active ? "true" : undefined}
-      aria-label={`${volume.mountPoint} ${volume.name}`}
+      aria-label={volume.name}
       onClick={open}
       onKeyDown={activateOnKey(open)}
       onContextMenu={onContextMenu}
@@ -43,9 +43,7 @@ const VolumeItem = ({
     >
       <Icon icon={volumeIcon(volume)} />
       <div className="details">
-        <p>
-          <span>{volume.mountPoint}</span> {volume.name}
-        </p>
+        <p>{volume.name}</p>
         <UsageBar percentage={volume.diskUsage.percentage} />
         {settings.showVolumeSize && (
           <p className="drive_size">
@@ -76,7 +74,7 @@ const VolumeItem = ({
   return collapsed ? (
     <Tooltip
       contents
-      label={`${volume.mountPoint} ${volume.name}`}
+      label={volume.name}
       placement={TOOLTIP_PLACEMENT.RIGHT}
     >
       {row}
